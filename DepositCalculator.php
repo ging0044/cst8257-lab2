d401253e03b4797135acfe637ac1794ea3a3620e
Patrick Gingras <ging0044@algonquinlive.com>
2017-09-15 15:04:26 -0400

    Styled with Bootstrap 4

--- DepositCalculator.php
+++ DepositCalculator.php
@@ -1,6 +1,8 @@
 <html>
     <head>
         <title>Deposit Calculator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css">
         <?php
 
 /* 
